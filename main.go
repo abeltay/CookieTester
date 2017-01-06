e8e651c9f23a4df8f372f2e93e024b3b81fbbec6
Abel Tay <abel.tay@gmail.com>
2017-01-06 11:10:16 +0800

    Add handler to test deletion of cookies

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/api/set", SetCookieHandler)
 	mux.HandleFunc("/get", GetCookieHandler)
+	mux.HandleFunc("/delete", DeleteCookieHandler)
 	log.Fatal(http.ListenAndServe(":8000", mux))
 }
 
@@ -33,4 +34,17 @@ func SetCookieHandler(w http.ResponseWriter, r *http.Request) {
 
 	http.SetCookie(w, &c)
 	fmt.Fprintf(w, "<html><head><title>Create Cookie</title></head><body>Cookie Created</body></html>")
+}
+
+func DeleteCookieHandler(w http.ResponseWriter, r *http.Request) {
+	c := http.Cookie{
+		Name:     "_session_id",
+		MaxAge:   -1,
+		HttpOnly: true,
+		Domain:   ".lvh.me",
+		Path:     "/",
+	}
+
+	http.SetCookie(w, &c)
+	fmt.Fprintf(w, "<html><head><title>Delete Cookie</title></head><body>Cookie Deleted</body></html>")
 }
